30ec35f647ba142bb90d382e224a4e2457aa87c1
Sarang Chehrehsa <Sarang.Chehrehsa@southerncross.co.nz>
2019-06-28 10:48:23 +1200

    cleaned up

--- init.py
+++ init.py
@@ -12,14 +12,6 @@ api = Api(app)
 helper = Helper()
 tm = TopicModel('model/model.p', 'model/dictionary.p')
 
-
-# ask_req_fields = api.model('AskReqFields', {
-#     'sid': fields.String(required=True, description='The session ID of the message'),
-#     'question': fields.String(required=True, description='The question that should be answered'), 
-#     'debug': fields.Boolean(default=False, description='The debug mode for getting extra parameters from the answer'), 
-#     'google_search': fields.Boolean(default=True, description='By setting to false, will not search google anymore')
-# })
-
 sentiment_req_fields = api.model('SentimentReqFields', {
     'text': fields.String(required=True, description='The user conversations'), 
     'uid': fields.String(required=True, description='The user ID')
